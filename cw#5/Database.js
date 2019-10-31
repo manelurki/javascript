409e371e9f8673684fae9de36df594acc310eee5
manelurki <50777168+manelurki@users.noreply.github.com>
2019-10-31 14:23:59 +0300

    Add files via upload

---
+++ cw#5/Database.js
@@ -0,0 +1,40 @@
+
+
+
+class Course
+{
+    constructor(name,time,date,rooms)
+    {
+        this.name=name;
+        this.time=time;
+        this.date=date;
+        this.rooms=rooms;
+    }
+    toString()
+    {
+        return this.name + ""
+    }
+}
+
+class Student
+{
+    constructor(id,name,gpa)
+    {
+        this.id=id;
+        this.name=name;
+        this.gpa=gpa;
+        this.courses=[]
+    }
+    addCourse(c)
+    {
+        this.courses.push(c)
+    }
+    toString()
+    {
+        return this.id +""
+        
+    }
+    
+}
+
+
